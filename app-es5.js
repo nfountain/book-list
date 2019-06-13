02888e848fe5243ef9bbf1987cbf0e316ee6825d
Nicena Fontaine <nicenas.email@gmail.com>
2019-06-13 15:04:12 -0400

    Add function to append a user-entered book to the list.
    Add subtle adjustments to styles for links/hovering over links.

--- app-es5.js
+++ app-es5.js
@@ -12,6 +12,24 @@ function Book(title, author, isbn) {
 // prototype methods (add book, delete book, show alert) will be added to this constructor
 function UI() {}
 
+// Prototype Methods
+UI.prototype.addBookToList = function(book) {
+  // console.log(book); // returns Book {title: "The funniest book ever", author: "Denise Netterfield", isbn: "1-23-4567890"}
+  const list = document.getElementById('book-list');
+  // create tbale row with new Book
+  const row = document.createElement('tr');
+  // console.log(row);
+  // insert columns
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</a></td>
+  `;
+  // append the row to the tbody that was not populated in the html
+  list.appendChild(row);
+};
+
 // Event Listeners
 bookForm.addEventListener('submit', function(e) {
   // console.log('works?'); // YAY! test is successful
@@ -26,5 +44,12 @@ bookForm.addEventListener('submit', function(e) {
   const book = new Book(title, author, isbn);
   // console.log(book); // returns the book object with user entered values
   // NOTE: book is not globally accessible outside of this anonymous function
+
+  // Instantiate userInterface
+  const userInterface = new UI(); // referring to the UI constructor on line 13
+
+  // Add book to list
+  userInterface.addBookToList(book);
+
   e.preventDefault();
 });
